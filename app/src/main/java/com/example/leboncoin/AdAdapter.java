ab19ed3cdff2f4b12c38247786904a77725961b4
Alkoos <duval.max02@gmail.com>
2024-02-16 11:09:12 +0100

    début appli

---
+++ app/src/main/java/com/example/leboncoin/AdAdapter.java
@@ -0,0 +1,47 @@
+package com.example.leboncoin;
+
+import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class AdAdapter extends BaseAdapter {
+    private final Context context;
+    private final ArrayList<AdModel> adModelArrayList;
+    private final LayoutInflater inflater;
+
+    public AdAdapter (Context context,ArrayList<AdModel> adModelArrayList){
+        this.context=context;
+        this.adModelArrayList=adModelArrayList;
+        inflater = (LayoutInflater.from(context));
+    }
+    @Override
+    public int getCount() {return adModelArrayList.size();}
+    @Override
+    public Object getItem(int i){return adModelArrayList.get(i);}
+    @Override
+    public long getItemId(int i) { return i ; }
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup){
+        AdModel ad = adModelArrayList.get(i);
+        view = inflater.inflate(R.layout.item_listview_ad,null);
+
+        ImageView imageTV = view.findViewById(R.id.image_ad);
+        TextView titleTV = view.findViewById(R.id.title_ad);
+        TextView addressTV = view.findViewById(R.id.adress_ad);
+
+        imageTV.setImageResource(ad.getImage());
+        titleTV.setText(ad.getTitle());
+        addressTV.setText(ad.getAddress());
+        return view;
+    }
+}
--- app/src/main/java/com/example/leboncoin/AdListViewActivity.java
+++ app/src/main/java/com/example/leboncoin/AdListViewActivity.java
@@ -4,20 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.text.TextRunShaper;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdListViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AdModel test =  new AdModel("test","test",1);
+        setContentView(R.layout.activity_ad_list_view);
+        AdModel test =  new AdModel("test","test",R.drawable.ic_launcher_background);
+        AdModel test2 =  new AdModel("test2","test2",R.drawable.crescentcity3);
+        ArrayList<AdModel> liste = new ArrayList<AdModel>();
+        liste.add(test);
+        liste.add(test2);
 
-        LinearLayout B= new LinearLayout(this);
-        TextView e = new TextView(this);
-        e.setText("Bonjour");
-        B.addView(e);
-        setContentView(B);
+        AdAdapter adapter = new AdAdapter(this,liste);
+        ListView listview = findViewById(R.id.listview);
+        listview.setAdapter(adapter);
+
     }
 }
